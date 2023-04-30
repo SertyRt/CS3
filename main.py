81df4b25bdd91b52c6135552624b741c56152444
Jess <116273552+SertyRt@users.noreply.github.com>
2023-04-30 17:20:30 +0200

    from Lab

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
+if __name__ == '__main__':
 
-
-
-def Listen():
     list1 = list(range(1, 21))
     listSquare = [x**2 for x in list1]
     listEven = [x for x in list1 if x % 2 == 0]
 
 
-if __name__ == '__main__':
-    print("Liste")
+    print(listSquare + listEven + list1)
